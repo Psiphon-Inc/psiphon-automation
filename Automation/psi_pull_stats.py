e44e1045f5628b7ddc3a9a2f7f5a518366629f88
Rod Hynes <r.hynes@psiphon.ca>
2011-05-20 14:46:49 -0400

    Stubbed in more automation scripts

--- Automation/psi_pull_stats.py
+++ Automation/psi_pull_stats.py
@@ -17,15 +17,4 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import subprocess
-
-def build_client():
-    with open('build.cmd', 'w') as file:
-        file.write('call "C:\\Program Files\\Microsoft Visual Studio 10.0\\VC\\vcvarsall.bat" x86\n')
-        file.write('msbuild psiclient.sln /t:Rebuild /p:Configuration=Release')
-    return subprocess.call('build.cmd')
-
-if 0 == build_client():
-    print 'Python: SUCCESS'
-else:
-    print 'Python: FAIL'
+# TODO: script to pull stats from all servers
