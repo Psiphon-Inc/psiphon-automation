db2e24b2201fe84a8d7d2e71c500e974f990c624
Rod Hynes <r.hynes@psiphon.ca>
2011-06-21 15:50:26 -0400

    1. Added spreadsheet updating with unit test 2. Install script generates missing serevr values and updates spreadsheet 3. Install script invokes deploy for each unconfigured host

--- Automation/psi_install.py
+++ Automation/psi_install.py
@@ -18,26 +18,40 @@
 #
 
 import os
+import random
 import sys
 import textwrap
 import tempfile
+from OpenSSL import crypto
 
 import psi_ssh
+import psi_deploy
 
 sys.path.insert(0, os.path.abspath(os.path.join('..', 'Data')))
 import psi_db
 
-# if psi_build_config.py exists, load it and use psi_build_config.DATA_ROOT as the data root dir
+# if psi_build_config.py exists, load it and use psi_build_config.DATA_ROOT as
+# the data root dir
 
 if os.path.isfile('psi_data_config.py'):
     import psi_data_config
     psi_db.set_db_root(psi_data_config.DATA_ROOT)
 
-    
-#==============================================================================
 
-# We use an echo command over ssh to generate this file on the remote server to avoid
-# leaving IP Addresses on disk
+#==== Configuration ============================================================
+
+WEB_SERVER_SECRET_BYTE_LENGTH = 32
+SERVER_ID_WORD_LENGTH = 3
+SSL_CERTIFICATE_KEY_TYPE = crypto.TYPE_RSA
+SSL_KEY_SIZE = 2048
+SSL_DIGEST_TYPE = 'sha1'
+SSL_CERTIFICATE_VALIDITY = (60*60*24*365*10) # 10 years
+
+
+#==== Helpers ==================================================================
+
+# We use an echo command over ssh to generate this file on the remote server
+# to avoid leaving IP Addresses on disk
 def make_xl2tpd_config_file_command(server_index, ip_address):
     file_contents = textwrap.dedent('''
         [global]
@@ -54,7 +68,8 @@ def make_xl2tpd_config_file_command(server_index, ip_address):
         length bit = yes
         ''' % (ip_address, server_index, server_index, server_index))
 
-    return 'echo "%s" > /etc/xl2tpd/xl2tpd%d.conf' % (file_contents.replace('\n', '\\\n'), server_index)
+    return 'echo "%s" > /etc/xl2tpd/xl2tpd%d.conf' % (
+        file_contents.replace('\n', '\\\n'), server_index)
 
 def make_xl2tpd_options_file_command():
     file_contents = textwrap.dedent('''
@@ -73,18 +88,22 @@ def make_xl2tpd_options_file_command():
         proxyarp
         connect-delay 5000
         ''')
-        
-    return 'echo "%s" > /etc/ppp/options.xl2tpd' % (file_contents.replace('\n', '\\\n'),)
-    
+
+    return 'echo "%s" > /etc/ppp/options.xl2tpd' % (
+        file_contents.replace('\n', '\\\n'),)
+
 def make_xl2tpd_chap_secrets_command():
     file_contents = '*   *   password    *'
-    return 'echo "%s" > /etc/ppp/chap-secrets' % (file_contents.replace('\n', '\\\n'),)
-    
-# This file is written to disk then copied to the remote server.  We had problems echoing it out
-# to a file in an ssh command, probably because of all the special characters.  It's OK to write this
-# to disk locally, because there isn't any secret info (like IP Addresses) here.
+    return 'echo "%s" > /etc/ppp/chap-secrets' % (
+        file_contents.replace('\n', '\\\n'),)
+
+# This file is written to disk then copied to the remote server.  We had
+# problems echoing it out to a file in an ssh command, probably because of all
+# the special characters.  It's OK to write this to disk locally, because there
+# isn't any secret info (like IP Addresses) here.
 def make_xl2tpd_initd_file(server_count):
-    # TODO: use textwrap.dedent and try initial_indent to line up the repeated segments
+    # TODO: use textwrap.dedent and try initial_indent to line up the
+    # repeated segments
     file_contents = '''
 #! /bin/sh
 
@@ -147,16 +166,78 @@ esac
 
 exit 0
 '''
-    
+
     return file_contents
 
-    
+
+def generate_web_server_secret():
+    return os.urandom(WEB_SERVER_SECRET_BYTE_LENGTH)
+
+
+def generate_server_id():
+    WORDS = '''
+multion            moding             addrer             hactrogram
+emaging            keyboot            forsonic           trogram
+reless             syster             hareware           accense
+lickbash           randowser          reacy              trate
+rany               typers             zinink             propree
+acting             malet              equicess           datink
+docasion           horer              tabyte             filer
+mems               numerl             kers               actocumerl
+inse               sectime            quican             documedia
+electrows          mon-col            explicktion        rookie
+pirtus             noteboad           date               pritop
+sprine             uplore             typer              systers
+filet              printu             disual             shat
+prows              lican              adwall             nonink
+nonal              serce              modia              vireacy
+apams              datos              prive              keyboad
+relent             ubunix             allocurce          files
+redia              enity              reasing            instem
+dowser             sharing            computem           sectrogram
+kernet             hored              sectrojan          engual
+haress             deboad             megramputem        mource
+intu               explork            supg               docurch
+opers              megram             inter              redinic
+sourite            prothon            licent             sernet
+morer              applory            pyte               mareshat
+'''.split()
+    return ' '.join([random.choice(WORDS) for _ in range(SERVER_ID_WORD_LENGTH)])
+
+
+def generate_self_signed_certificate():
+    key_pair = crypto.PKey()
+    key_pair.generate_key(SSL_KEY_TYPE, SSL_KEY_SIZE)
+    request = crypto.X509Req()
+    #
+    # TODO: generate a random, yet plausible DN
+    #
+    # subject = request.get_subject()
+    # for (key, value) in subject_pairs.items():
+    #    setattr(subject, key, value)
+    #
+    request.set_pubkey(key_pair)
+    request.sign(key_pair, SSL_DIGEST_TYPE)
+    certificate = crypto.X509()
+    certificate.set_version(2)
+    certificate.set_serial_number(0)
+    certificate.gmtime_adj_notBefore(0)
+    certificate.gmtime_adj_notAfter(SSL_CERTIFICATE_VALIDITY)
+    certificate.set_issuer(request.get_subject())
+    certificate.set_subject(request.get_subject())
+    certificate.set_pubkey(request.get_pubkey())
+    certificate.sign(key_pair, SSL_DIGEST_TYPE)
+    return (crypto.dump_privatekey(crypto.FILETYPE_PEM, key_pair),
+            crypto.dump_certificate(crypto.FILETYPE_PEM, certificate))
+
+
 #==============================================================================
-    
+
+
 if __name__ == "__main__":
 
     # Install each server that only has partially filled-in data.
-    # We don't want to do anything on hosts that are fully configured, 
+    # We don't want to do anything on hosts that are fully configured,
     # and for partially configured hosts we need to completely reconfigure
     # all files because we use a counter in the xl2tpd config files that is not
     # directly tied to the IP Address in the database.
@@ -164,32 +245,79 @@ if __name__ == "__main__":
     # install a complete configuration on those hosts.
 
     unconfigured_servers = [server for server in psi_db.get_servers()
-                            if server.Web_Server_Secret is None or
-                            server.Web_Server_Certificate is None or
-                            server.Web_Server_Private_Key is None]
-    
+                            if server.Server_ID is None or
+                               server.Web_Server_Secret is None or
+                               server.Web_Server_Certificate is None or
+                               server.Web_Server_Private_Key is None]
+
     unconfigured_host_ids = set([server.Host_ID for server in unconfigured_servers])
 
-    unconfigured_hosts = [host for host in psi_db.get_hosts() if host.Host_ID in unconfigured_host_ids]
-    
+    unconfigured_hosts = [host for host in psi_db.get_hosts()\
+                          if host.Host_ID in unconfigured_host_ids]
+
     for host in unconfigured_hosts:
         ssh = psi_ssh.SSH(
                 host.IP_Address, host.SSH_Username,
                 host.SSH_Password, host.SSH_Host_Key)
-                
+
         # Generate and upload xl2tpd config files and init script
-        host_servers = [server for server in psi_db.get_servers() if server.Host_ID == host.Host_ID]
+
+        host_servers = [server for server in psi_db.get_servers()\
+                        if server.Host_ID == host.Host_ID]
+
         for index, server in enumerate(host_servers):
             ssh.exec_command(make_xl2tpd_config_file_command(index, server.IP_Address))
-        
+
         ssh.exec_command(make_xl2tpd_options_file_command())
-        
+
         ssh.exec_command(make_xl2tpd_chap_secrets_command())
-        
+
         file = tempfile.NamedTemporaryFile(delete=False)
         file.write(make_xl2tpd_initd_file(len(host_servers)))
         file.close()
         ssh.put_file(file.name, '/etc/init.d/xl2tpd')
-        os.unlink(file.name)
-    
-    
+        os.remove(file.name)
+
+        # Generate unique server alias and web server credentials
+        # Values are stored in updated database; upload happens in deploy below
+
+        server_database_updates = []
+
+        for server in [server for server in host_servers\
+                       if server.Server_ID is None or
+                          server.Web_Server_Secret is None or
+                          server.Web_Server_Certificate is None or
+                          server.Web_Server_Private_Key is None]:
+
+            existing_ids = [server.Server_ID for server in psi_db.get_servers()]
+            while True:
+                server_id = generate_server_id()
+                if not server_id in existing_ids:
+                    break
+
+            web_server_secret = generate_web_server_secret()
+
+            # Generated output is PEM, e.g.,
+            #
+            # '-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEAvAmIPX5kzPz...
+            #  EZ3bCbVPQNP6ZnC6EONGuGTDgTTU30\n-----END RSA PRIVATE KEY-----\n'
+            #
+            # We strip the BEGIN/END lines in the database format.
+
+            key_pem, cert_pem = generate_self_signed_certificate()
+            key = key_pem.split('\n')[1]
+            cert = cert.pem.split('\n')[1]
+
+            # Reference update rows by IP address
+            server_database_updates.append(
+                ((u'IP_Address', server.IP_Address),
+                 [(u'Server_ID', server_id),
+                  (u'Web_Server_Secret', web_server_secret),
+                  (u'Web_Server_Certificate', cert),
+                  (u'Web_Server_Private_Key', key)]))
+
+        # Update server rows in database
+        psi_db.update_servers(server_database_updates)
+
+        # Deploy will upload web server source database data and client builds
+        psi_deploy.deploy(host)
