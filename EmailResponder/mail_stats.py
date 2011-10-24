c7264b371e3423ad1f536ecd1dca8e70444649dd
apritchard <a.pritchard@psiphon.ca>
2011-10-24 16:44:41 -0400

    Added stats mailing tool

---
+++ EmailResponder/mail_stats.py
@@ -0,0 +1,60 @@
+# Copyright (c) 2011, Psiphon Inc.
+# All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+'''
+Intended to be run as a cron job to send SES quota and stats to desired email
+address (using SES itself). Credentials for SES are expected to be in boto 
+conf file or environment variables.
+'''
+
+import json
+from boto.ses.connection import SESConnection
+
+import sendmail
+
+
+RECIPIENT_ADDRESS = 'mail@example.com'
+SENDER_ADDRESS = 'Psiphon Responder <get@psiphon3.com>'
+
+
+if __name__ == '__main__':
+
+    # Open the connection. Uses creds from boto conf or env vars.
+    conn = SESConnection()
+
+    quota = conn.get_send_quota()
+    stats = conn.get_send_statistics()
+
+    email_body = ''
+    email_body += json.dumps(quota, indent=2)
+    email_body += '\n\n'
+    email_body += json.dumps(stats, indent=2)
+
+    subject = '[MailResponder] Stats'
+
+    raw_email = sendmail.create_raw_email(RECIPIENT_ADDRESS, 
+                                          SENDER_ADDRESS, 
+                                          subject, 
+                                          email_body)
+
+    if not raw_email:
+        exit(1)
+
+    if not sendmail.send_raw_email_amazonses(raw_email, SENDER_ADDRESS):
+        exit(1)
+
+    exit(0)
+
