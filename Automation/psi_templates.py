f3ef97434df83debe753351efc944b0a22c3bcd3
Adam Kruger <a.kruger@psiphon.ca>
2011-11-22 13:51:56 -0500

    Add ltr div around ltr blocks.

--- Automation/psi_templates.py
+++ Automation/psi_templates.py
@@ -47,9 +47,11 @@ Türkmençe - {0}/tk.html
 
 def get_html_email_content(s3_bucket_name):
     bucket_root_url = 'https://s3.amazonaws.com/' + s3_bucket_name
-    return '''To use Psiphon 3, save the attached file with a .exe extension.<br>
+    return '''<div style="direction: ltr;">
+To use Psiphon 3, save the attached file with a .exe extension.<br>
 Or click the link below.<br>
 <a href="{0}/en.html">{0}/en.html</a><br>
+</div>
 <br>
 <div style="direction: rtl;">
 برای استفاده از سایفون ۳، فایل پیوست شده را با پسوند زیر ذخیره کنید‪:‬<br>
@@ -58,10 +60,14 @@ Or click the link below.<br>
 <a href="{0}/fa.html">{0}/fa.html</a><br>
 </div>
 <br>
+<div style="direction: ltr;">
 Русский - <a href="{0}/ru.html">{0}/ru.html</a><br>
 Türkmençe - <a href="{0}/tk.html">{0}/tk.html</a><br>
 中文 - <a href="{0}/zh.html">{0}/zh.html</a><br>
+</div>
 <div style="direction: rtl;">
 العربي - <a href="{0}/ar.html">{0}/ar.html</a><br>
 </div>
-ภาษาไทย - <a href="{0}/th.html">{0}/th.html</a><br>'''.format(bucket_root_url)
+<div style="direction: ltr;">
+ภาษาไทย - <a href="{0}/th.html">{0}/th.html</a><br>
+</div>'''.format(bucket_root_url)
