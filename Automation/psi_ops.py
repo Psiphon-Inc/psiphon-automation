b5d39c55d9eaaa8e3c1c23a49ad6a68cbb0225f4
Rod Hynes <r.hynes@psiphon.ca>
2011-11-24 17:19:52 -0500

    Fixed 'None' region problem in handshake home pages

--- Automation/psi_ops.py
+++ Automation/psi_ops.py
@@ -813,7 +813,7 @@ class PsiphonNetwork(psi_ops_cms.PersistentObject):
         if region in sponsor.home_pages:
             sponsor_home_pages = [home_page.url for home_page in sponsor.home_pages[region]]
         # case: lookup failed or no corresponding region home page found --> use default
-        if not sponsor_home_pages and None in sponsor.home_pages:
+        if not sponsor_home_pages and 'None' in sponsor.home_pages:
             sponsor_home_pages = [home_page.url for home_page in sponsor.home_pages[None]]
         return sponsor_home_pages
     
