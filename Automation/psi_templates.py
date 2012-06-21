c81b456a469642babdd2fdcea62f1cea13b1e6c3
Rod Hynes <r.hynes@psiphon.ca>
2012-06-21 15:07:46 -0400

    Created per-language YAML files for all email strings; refactored psi_templates

--- Automation/psi_templates.py
+++ Automation/psi_templates.py
@@ -18,6 +18,35 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+import os
+import yaml
+
+LANGUAGES = [
+    'en',
+    'fa',
+    'ar',
+    'zh',
+    'uz@cyrillic',
+    'uz@Latn',
+    'ru',
+    'kk',
+    'az',
+    'tk',
+    'th',
+    'ug@Latn'
+]
+
+def get_language_string(language, key):
+    path = os.path.join('.', 'TemplateStrings', language + '.yaml')
+    with open(path) as f:
+        str = yaml.load(f.read())[key]
+    # Assumes strings have one {0} format specifier to receive the language name
+    # Other format specifiers, to be substituted later, should be escaped: {{N}}
+    return str.format(language)
+
+def get_all_languages_string(key):
+    return ''.join([get_language_string(language, key) for language in LANGUAGES])
+
 def get_tweet_message(s3_bucket_name):
     bucket_root_url = 'https://s3.amazonaws.com/' + s3_bucket_name
     return 'Get Psiphon 3 here: %s/en.html' % (bucket_root_url,)
@@ -25,165 +54,33 @@ def get_tweet_message(s3_bucket_name):
 def get_plaintext_email_content(
         s3_bucket_name):
     bucket_root_url = 'https://s3.amazonaws.com/' + s3_bucket_name
-    return '''English - {0}/en.html
-فارسی - {0}/fa.html
-中文 - {0}/zh.html
-Ўзбекча - {0}/uz@cyrillic.html
-O'zbekcha - {0}/uz@Latn.html
-Русский - {0}/ru.html
-қазақ тілі - {0}/kk.html
-azərbaycan dili - {0}/az.html
-Türkmençe - {0}/tk.html
-العربي - {0}/ar.html
-ภาษาไทย - {0}/th.html
-Uyghurche - {0}/ug@Latn.html
-'''.format(bucket_root_url)
-
+    return get_all_languages_string(
+        'plaintext_email_no_attachment').format(bucket_root_url)
 
 def get_html_email_content(
         s3_bucket_name):
     bucket_root_url = 'https://s3.amazonaws.com/' + s3_bucket_name
-    return '''<div style="direction: ltr;">
-English - <a href="{0}/en.html">{0}/en.html</a><br>
-</div>
-<div style="direction: rtl;">
-فارسی - <a href="{0}/fa.html">{0}/fa.html</a><br>
-</div>
-<div style="direction: ltr;">
-中文 - <a href="{0}/zh.html">{0}/zh.html</a><br>
-Ўзбекча - <a href="{0}/uz@cyrillic.html">{0}/uz@cyrillic.html</a><br>
-O'zbekcha - <a href="{0}/uz@Latn.html">{0}/uz@Latn.html</a><br>
-Русский - <a href="{0}/ru.html">{0}/ru.html</a><br>
-қазақ тілі - <a href="{0}/kk.html">{0}/kk.html</a><br>
-azərbaycan dili - <a href="{0}/az.html">{0}/az.html</a><br>
-Türkmençe - <a href="{0}/tk.html">{0}/tk.html</a><br>
-</div>
-<div style="direction: rtl;">
-العربي - <a href="{0}/ar.html">{0}/ar.html</a><br>
-</div>
-<div style="direction: ltr;">
-ภาษาไทย - <a href="{0}/th.html">{0}/th.html</a><br>
-Uyghurche - <a href="{0}/ug@Latn.html">{0}/ug@Latn.html</a><br>
-</div>
-'''.format(bucket_root_url)
+    return get_all_languages_string(
+        'html_email_no_attachment').format(bucket_root_url)
 
 def get_plaintext_attachment_email_content(
         s3_bucket_name,
         windows_attachment_filename,
         android_attachment_filename):
     bucket_root_url = 'https://s3.amazonaws.com/' + s3_bucket_name
-    return '''To use Psiphon 3 for Windows, save the attached file {1} with a ".exe" extension.
-For more information or to download the files again, click the link below.
-{0}/en.html
-
-برای استفاده از سایفون ۳، فایل پیوست شده را {1} با پسوند زیر ذخیره کنید:
-.exe
-برای اطلاعات بیشتر یا دانلود دوباره فایل ها، لطفآ روی لینک زیرکلیک کنید.
-{0}/fa.html
-
-要使用Psiphon 3, 保存文件.exe扩展名。
-或者点击下面的链接。
-{0}/zh.html
-
-لاستخدام برنامج Psiphon 3 لنظام التشغيل Windows، قم بحفظ الملف المرفق {1} ذو الامتداد ".exe".
-لمزيد من المعلومات أو لإعادة تنزيل الملفات، انقر الارتباط أدناه.
-{0}/ar.html
-
-Psiphon 3 дан фойдаланиш учун илова қилинган файлни .exe кенгайтиришда сақланг.
-Ёки қуйидаги уланишни босинг. 
-{0}/uz@cyrillic.html
-
-Psiphon 3 dan foydalanish uchun ilova qilingan faylni .exe kyengaytirishda saqlang.
-Yoki quyidagi ulanishni bosing.
-{0}/uz@Latn.html
-
-Psiphon 3 для Windows - сохраните прикрепленный файл {1} с расширением ".exe".
-Для получения дополнительной информации или скачивания файлов, кликните на ссылку:
-{0}/ru.html
-
-Psiphon 3 құралын қолдану үшін тіркелген файлды.exe пішімінде сақтаңыз.
-Немесе төмендегі сілтемені басыңыз.
-{0}/kk.html
-
-Psiphon 3-dən istifadə etmək üçün .exe genişlənməsində olan qoşma faylını saxlayın.
-Və ya aşağıdakı əlaqəyə klikləyin.
-{0}/az.html
-
-Türkmençe - {0}/tk.html
-ภาษาไทย - {0}/th.html
-Uyghurche - {0}/ug@Latn.html'''.format(
-    bucket_root_url,
-    windows_attachment_filename,
-    android_attachment_filename)
+    return get_all_languages_string(
+        'plaintext_email_with_attachment').format(
+            bucket_root_url,
+            windows_attachment_filename,
+            android_attachment_filename)
 
 def get_html_attachment_email_content(
         s3_bucket_name,
         windows_attachment_filename,
         android_attachment_filename):
     bucket_root_url = 'https://s3.amazonaws.com/' + s3_bucket_name
-    return '''<div style="direction: ltr;">
-To use Psiphon 3 for Windows, save the attached file {1} with a ".exe" extension.<br>
-For more information or to download the files again, click the link below.<br>
-<a href="{0}/en.html">{0}/en.html</a><br>
-</div>
-<br>
-<div style="direction: rtl;">
-برای استفاده از سایفون ۳، فایل پیوست شده را {1} با پسوند زیر ذخیره کنید:<br>
-.exe<br>
-برای اطلاعات بیشتر یا دانلود دوباره فایل ها، لطفآ روی لینک زیرکلیک کنید.<br>
-<a href="{0}/fa.html">{0}/fa.html</a><br>
-</div>
-<br>
-<div style="direction: ltr;">
-要使用Psiphon 3, 保存文件.exe扩展名。<br>
-或者点击下面的链接。<br>
-<a href="{0}/zh.html">{0}/zh.html</a><br>
-</div>
-<br>
-<br>
-<div style="direction: rtl;">
-لاستخدام برنامج Psiphon 3 لنظام التشغيل Windows، قم بحفظ الملف المرفق {1} ذو الامتداد ".exe".
-لمزيد من المعلومات أو لإعادة تنزيل الملفات، انقر الارتباط أدناه.
-<a href="{0}/ar.html">{0}/ar.html</a><br>
-</div>
-<br>
-<div style="direction: ltr;">
-Psiphon 3 дан фойдаланиш учун илова қилинган файлни .exe форматида сақланг.<br>
-Ёки мана бу линкни босинг.<br> 
-<a href="{0}/uz@cyrillic.html">{0}/uz@cyrillic.html</a><br>
-</div>
-<br>
-<div style="direction: ltr;">
-Psiphon 3 dan foydalanish uchun ilova qilingan faylni .exe formatida saqlang.<br>
-Yoki mana bu linkni bosing.<br> 
-<a href="{0}/uz@Latn.html">{0}/uz@Latn.html</a><br>
-</div>
-<br>
-<div style="direction: ltr;">
-Psiphon 3 для Windows - сохраните прикрепленный файл {1} с расширением ".exe".<br>
-Для получения дополнительной информации или скачивания файлов, кликните на ссылку: {0}/ru.html<br>
-<a href="{0}/ru.html">{0}/ru.html</a><br>
-</div>
-<br>
-<div style="direction: ltr;">
-Psiphon 3 құралын қолдану үшін тіркелген файлды.exe пішімінде сақтаңыз.<br>
-Немесе төмендегі сілтемені басыңыз.<br> 
-<a href="{0}/kk.html">{0}/kk.html</a><br>
-</div>
-<br>
-<div style="direction: ltr;">
-Psiphon 3-dən istifadə etmək üçün .exe genişlənməsində olan qoşma faylını saxlayın.<br>
-Və ya aşağıdakı əlaqəyə klikləyin.<br> 
-<a href="{0}/az.html">{0}/az.html</a><br>
-</div>
-<br>
-<div style="direction: ltr;">
-Türkmençe - <a href="{0}/tk.html">{0}/tk.html</a><br>
-</div>
-<div style="direction: ltr;">
-ภาษาไทย - <a href="{0}/th.html">{0}/th.html</a><br>
-Uyghurche - <a href="{0}/ug@Latn.html">{0}/ug@Latn.html</a><br>
-</div>'''.format(
-    bucket_root_url,
-    windows_attachment_filename,
-    android_attachment_filename)
+    return get_all_languages_string(
+        'html_email_with_attachment').format(
+            bucket_root_url,
+            windows_attachment_filename,
+            android_attachment_filename)
