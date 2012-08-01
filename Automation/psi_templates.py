f39db2c20fc15b89acc86efc88ec4f7c530e547b
efryntov <none@none>
2012-08-01 14:15:14 -0400

    YAML translation resources updated
    --HG--
    branch : transifex-yaml

--- Automation/psi_templates.py
+++ Automation/psi_templates.py
@@ -39,7 +39,7 @@ LANGUAGES = [
 def get_language_string(language, key):
     path = os.path.join('.', 'TemplateStrings', language + '.yaml')
     with open(path) as f:
-        str = yaml.load(f.read())[key]
+        str = yaml.load(f.read())[language][key]
     # Assumes strings have one {0} format specifier to receive the language name
     # Other format specifiers, to be substituted later, should be escaped: {{N}}
     return str.format(language)
