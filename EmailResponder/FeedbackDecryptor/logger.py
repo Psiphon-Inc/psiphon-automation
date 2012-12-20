1685c958461c6ad0502b1ffbb2a6afdf45e59623
Adam Pritchard <a.pritchard@psiphon.ca>
2012-12-20 16:55:57 -0500

    Added executable name to syslog entries
    --HG--
    branch : diagnostic-feedback-windows

--- EmailResponder/FeedbackDecryptor/logger.py
+++ EmailResponder/FeedbackDecryptor/logger.py
@@ -20,6 +20,7 @@ properly on Windows.
 '''
 
 import os
+import sys
 import logging
 import logging.handlers
 
@@ -29,14 +30,19 @@ _my_logger = logging.getLogger('MyLogger')
 _my_logger.setLevel(logging.DEBUG if _DEBUG else logging.WARNING)
 _my_logger.addHandler(logging.handlers.SysLogHandler(address='/dev/log'))
 
+# Before Python 3.3, there is no way to specify a "tag" or "ident" to syslog
+# entries. So we'll hack it in manually.
+# Ref: http://docs.python.org/dev/library/logging.handlers.html#logging.handlers.SysLogHandler.emit
+_main = sys.modules['__main__'].__file__ if hasattr(sys.modules['__main__'], '__file__') else 'feedbackdecryptor_service'
+
 
 def debug_log(s):
-    _my_logger.debug(s)
+    _my_logger.debug('%s: %s' % (_main, s))
 
 
 def log(s):
-    _my_logger.critical(s)
+    _my_logger.critical('%s: %s' % (_main, s))
 
 
 def exception(s=''):
-    _my_logger.exception(s)
+    _my_logger.exception('%s: %s' % (_main, s))
