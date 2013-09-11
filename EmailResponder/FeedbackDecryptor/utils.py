92337585def97dfb6c4f06050629252a823f0815
Kruger <none@none>
2013-09-11 16:12:15 +0000

    Add template_android_3.mako (adds is_playstore_build).

--- EmailResponder/FeedbackDecryptor/utils.py
+++ EmailResponder/FeedbackDecryptor/utils.py
@@ -200,7 +200,7 @@ def is_diagnostic_info_sane(obj):
     exemplar = {
                 'Metadata': {
                              'platform': lambda val: val in ['android', 'windows'],
-                             'version': lambda val: val in range(1, 3),
+                             'version': lambda val: val in range(1, 4),
                              'id': lambda val: re.match(r'^[a-fA-F0-9]{16}', val) is not None
                              },
 
