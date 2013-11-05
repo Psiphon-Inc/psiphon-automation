e4809908786c6fcc6c33607da9fd131b6dbf6ed3
mfallone <none@none>
2013-11-05 12:49:50 -0500

    bugfixes
    --HG--
    branch : digitalocean-api

--- Automation/psi_digitalocean.py
+++ Automation/psi_digitalocean.py
@@ -70,13 +70,13 @@ def stop_droplet(droplet_id,
                                                                      digitalocean_account.api_key)):
     print 'Powering down droplet: %s' % (droplet_id)
     droplet_off = check_response(do_api.droplet_power_off(droplet_id))
-    if not wait_on_event_completion(do_api, droplet_off['event_id'], interval=30):
+    if not wait_on_event_completion(do_api, droplet_off['event_id'], interval=60):
         droplet_state = check_response(do_api.droplet_show(droplet_id))['droplet']['status']
         if droplet_state == 'off':
             return True
         else:
             print 'Problem powering droplet off, droplet state: %s' % (str(droplet_state))
-            raise
+            raise Exception
 
 def start_droplet(droplet_id,
                   do_api=digitalocean.DigitalOceanAPI.DigitalOceanAPI(digitalocean_account.client_id,
@@ -89,7 +89,7 @@ def start_droplet(droplet_id,
             return True
         else:
             print 'Problem powering droplet on, droplet state: %s' (str(droplet_state))
-            raise
+            raise Exception
 
 def take_snapshot(digitalocean_account, droplet_id, snapshot_name={'name': None}):
     do_api = digitalocean.DigitalOceanAPI.DigitalOceanAPI(digitalocean_account.client_id,
@@ -102,8 +102,8 @@ def take_snapshot(digitalocean_account, droplet_id, snapshot_name={'name': None}
             print 'Powering down droplet: %s' % (droplet_id)
             stop_droplet(droplet_id, do_api)
         print 'Beginning snapshot'
-        resp = check_response(do_api.droplet_snapshot(droplet_id, snapshot_name, interval=60))
-        if not wait_on_event_completion(do_api, resp['event_id']):
+        resp = check_response(do_api.droplet_snapshot(droplet_id, snapshot_name))
+        if not wait_on_event_completion(do_api, resp['event_id'], interval=60):
             print 'Could not take snapshot'
             raise
         # We are not sure what state the droplet will be returned in. (one would assume the state it was previously in
@@ -222,8 +222,6 @@ def launch_new_server(digitalocean_account, default_params=True, use_public_imag
     except Exception as e:
         print 'Exception %s' % (str(e))
         raise
-    finally:
-        stop_droplet(digitalocean_account.base_id)
     
     return (image['name'], None, provider_id, droplet['ip_address'],
             digitalocean_account.base_ssh_port, 'root', new_root_password, 
