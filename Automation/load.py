fff27290d136bc9d27d0c852bd5190450f2e8210
mfallone <none@none>
2014-03-21 11:25:56 -0400

    added graphing of host load stats

--- Automation/load.py
+++ Automation/load.py
@@ -15,8 +15,12 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import ast
 import os
+import errno
+import shutil
 import sys
+import json
 import logging
 import pprint
 import operator
@@ -38,6 +42,7 @@ import psi_ops
 
 def check_load_on_host(host):
     try:
+        log_diagnostics('checking host: %s' % (host.id))
         users = g_psinet._PsiphonNetwork__count_users_on_host(host.id)
         load = g_psinet.run_command_on_host(host, 'uptime | cut -d , -f 4 | cut -d : -f 2 | awk -F \. \'{print $1}\'').strip()
         free = g_psinet.run_command_on_host(host, 'free | grep "buffers/cache" | awk \'{print $4/($3+$4) * 100.0}\'')
@@ -58,8 +63,10 @@ def check_load_on_hosts(psinet, hosts):
     pool = ThreadPool(25)
     global g_psinet
     g_psinet = psinet
+    log_diagnostics('Checking Hosts...')
     results = pool.map(check_load_on_host, hosts)
-    
+    log_diagnostics('...done checking hosts')
+
     for result in results:
         if result[1] == -1:
             # retry a failed host
@@ -99,6 +106,10 @@ def log_load():
         outfile.write('\n')
     send_mail(results)
 
+def log_diagnostics(line):
+    with open('psi_host_load_diagnostics.log', 'a') as log_file:
+        log_file.write(line + '\n')
+
 def send_mail(record):
     template_filename = 'psi_mail_hosts_load.mako'
     template_lookup = TemplateLookup(directories=[os.path.dirname(os.path.abspath('__file__'))]) 
@@ -114,8 +125,91 @@ def send_mail(record):
 
     # CSS in email HTML must be inline
     rendered = pynliner.fromString(rendered)
+    log_diagnostics('Sending email...')
     sender.send(config['statsEmailRecipients'], config['emailUsername'], 'Psiphon 3 Host Load Stats', repr(record), rendered)
+    log_diagnostics('Email sent.')
+
+
+FILENAME = 'psi_host_load_results.log'
+REPORTS_DIRNAME = 'host_reports'
+REPORTS_DIR = REPORTS_DIRNAME
+STAGING_DIR = os.path.join(REPORTS_DIR, 'staging')
+DATA_DIRNAME = 'data'
+DATA_DIR = os.path.join(STAGING_DIR, DATA_DIRNAME)
+REPORT_EXT = '.dat'
+FRESH_AGE = datetime.timedelta(2)
+REPORT_LIST_FILENAME = 'data.json'
+REPORT_LIST_FILEPATH = os.path.join(STAGING_DIR, REPORT_LIST_FILENAME)
+
+def dump_host_reports(fresh_hosts_only=True):
+    # Note: this routine keeps a file handle open per host; adjust ulimits accordlingly
+
+    # Delete any leftover file
+    shutil.rmtree(STAGING_DIR, ignore_errors=True)
+    _makedirs(STAGING_DIR)
+    _makedirs(DATA_DIR)
+
+    # Like { hostname: [host_fp, host_fname, max_date], ... }
+    host_files = {}
+
+    with open(FILENAME, 'r') as f:
+        for line in f:
+            data = ast.literal_eval(line)
+            start_time = data[0]
+            end_time = data[1]
+            loads = data[2]
+            if len(loads) != 3:
+                # old format, skip
+                continue
+            total_users = loads[0]
+            nonresponding_hosts = loads[1]
+            host_loads = loads[2]
+            for host_load in host_loads:
+                hostname =  host_load[0]
+                host_load = host_load[1][0]
+                if not hostname in host_files:
+                    fname = os.path.join(DATA_DIR, hostname + REPORT_EXT)
+                    host_files[hostname] = [open(fname, 'w'), fname, None]
+                                            
+                host_file = host_files[hostname]
+                host_file[0].write('%s,%s\n' % (end_time.replace(' ', 'T'), host_load))
+                host_file[2] = max(end_time, host_file[2])
+            # print end_time
+
+    # Collect data file info to put into the JSON index file
+    data_files = []
+
+    for host_file in host_files.itervalues():
+        host_file[0].close()
+        if fresh_hosts_only and datetime.datetime.strptime(host_file[2], '%Y-%m-%d %H:%M:%S.%f') + FRESH_AGE < datetime.datetime.now():
+            # Defunct. Delete.
+            os.unlink(host_file[1])
+        else:
+            fname = os.path.basename(host_file[1])
+            hostname = os.path.splitext(fname)[0]
+            data_files.append((hostname, './'+DATA_DIRNAME+'/'+fname))
+
+    # Dump the file list to a JSON index file.
+    with open(REPORT_LIST_FILEPATH, 'w') as listfile:
+        json.dump(data_files, listfile, indent=2)
+
+    # Move from staging to the final location
+    os.unlink(os.path.join(REPORTS_DIR, REPORT_LIST_FILENAME))
+    shutil.move(REPORT_LIST_FILEPATH, os.path.join(REPORTS_DIR, REPORT_LIST_FILENAME))
+    shutil.rmtree(os.path.join(REPORTS_DIR, DATA_DIRNAME))
+    shutil.move(DATA_DIR, os.path.join(REPORTS_DIR, DATA_DIRNAME))
+
+
+def _makedirs(path):
+    try:
+        os.makedirs(path)
+    except OSError as ex:
+        if ex.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
 
 if __name__ == "__main__":
     log_load()
-
+    dump_host_reports()
