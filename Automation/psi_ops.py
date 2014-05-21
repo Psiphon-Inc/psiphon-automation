c09e32adbc0c97bacf7364ffdd56a15372b84470
Kruger <none@none>
2014-05-21 10:56:19 -0400

    Upgrade existing server capabilities.
    --HG--
    branch : meek-experiment

--- Automation/psi_ops.py
+++ Automation/psi_ops.py
@@ -475,6 +475,13 @@ class PsiphonNetwork(psi_ops_cms.PersistentObject):
                 host.meek_server_fronting_host = None
                 host.meek_cookie_encryption_public_key = None
                 host.meek_cookie_encryption_private_key = None
+            for server in self.__servers.itervalues():
+                server.capabilities['FRONTED-MEEK'] = False
+                server.capabilities['UNFRONTED-MEEK'] = False
+            for server in self.__deleted_servers.itervalues():
+                server.capabilities['FRONTED-MEEK'] = False
+                server.capabilities['UNFRONTED-MEEK'] = False
+
             self.version = '0.26'
 
     def initialize_plugins(self):
