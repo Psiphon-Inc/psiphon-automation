17f7809dcb7ebdebf3fc932fa61c1a6bd15f531d
mfallone <none@none>
2015-03-13 16:13:11 -0400

    Updated python-digitalocean to latest, to reflect changes to DigitalOcean APIv2
    --HG--
    branch : digitalocean-apiv2

--- Automation/digitalocean_v2/digitalocean/SSHKey.py
+++ Automation/digitalocean_v2/digitalocean/SSHKey.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from .baseapi import BaseAPI
-import requests
+from .baseapi import BaseAPI, GET, POST, DELETE, PUT
+
 
 class SSHKey(BaseAPI):
     def __init__(self, *args, **kwargs):
@@ -21,32 +21,51 @@ class SSHKey(BaseAPI):
         return ssh_key
 
     def load(self):
-        data = self.get_data(
-            "account/keys/%s" % self.id,
-            type="GET"
-        )
+        """
+            Load the SSHKey object from DigitalOcean.
+
+            Requires either self.id or self.fingerprint to be set.
+        """
+        identifier = None
+        if self.id is not None:
+            identifier = self.id
+        elif self.fingerprint is not None:
+            identifier = self.fingerprint
+
+        data = self.get_data("account/keys/%s" % identifier, type=GET)
 
         ssh_key = data['ssh_key']
 
-        #Setting the attribute values
+        # Setting the attribute values
         for attr in ssh_key.keys():
-            setattr(self,attr,ssh_key[attr])
+            setattr(self, attr, ssh_key[attr])
         self.id = ssh_key['id']
+
+    def load_by_pub_key(self, public_key):
+        """
+            This method will laod a SSHKey object from DigitalOcean
+            from a public_key. This method will avoid problem like
+            uploading the same public_key twice.
+        """
+
+        data = self.get_data("account/keys/")
+        for jsoned in data['ssh_keys']:
+            if jsoned.get('public_key', "") == public_key:
+                self.id = jsoned['id']
+                self.load()
+                return self
+        return None
 
     def create(self):
         """
             Create the SSH Key
         """
         input_params = {
-                "name": self.name,
-                "public_key": self.public_key,
-            }
+            "name": self.name,
+            "public_key": self.public_key,
+        }
 
-        data = self.get_data(
-            "account/keys/",
-            type="POST",
-            params=input_params
-        )
+        data = self.get_data("account/keys/", type=POST, params=input_params)
 
         if data:
             self.id = data['ssh_key']['id']
@@ -56,13 +75,13 @@ class SSHKey(BaseAPI):
             Edit the SSH Key
         """
         input_params = {
-                "name": self.name,
-                "public_key": self.public_key,
-            }
+            "name": self.name,
+            "public_key": self.public_key,
+        }
 
         data = self.get_data(
             "account/keys/%s" % self.id,
-            type="PUT",
+            type=PUT,
             params=input_params
         )
 
@@ -73,10 +92,7 @@ class SSHKey(BaseAPI):
         """
             Destroy the SSH Key
         """
-        return self.get_data(
-            "account/keys/%s" % self.id,
-            type="DELETE",
-        )
+        return self.get_data("account/keys/%s" % self.id, type=DELETE)
 
     def __str__(self):
         return "%s %s" % (self.id, self.name)
