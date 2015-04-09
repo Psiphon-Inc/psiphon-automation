9eedec21372218875a6eec59cb844c1ac9370124
Kruger <none@none>
2015-04-09 09:21:12 -0400

    Sign go-core binary.

--- Automation/psi_ops_build_windows.py
+++ Automation/psi_ops_build_windows.py
@@ -38,6 +38,7 @@ EXECUTABLE_FILENAME = os.path.join(SOURCE_ROOT, 'Release', 'psiphon.exe')
 BUILDS_ROOT = os.path.join('.', 'Builds', 'Windows')
 BUILD_FILENAME_TEMPLATE = 'psiphon-%s-%s.exe'
 POLIPO_EXECUTABLE_FILENAME = os.path.join(SOURCE_ROOT, 'psiclient', '3rdParty', 'polipo.exe')
+CORE_EXECUTABLE_FILENAME = os.path.join(SOURCE_ROOT, 'psiclient', '3rdParty', 'psiphon-tunnel-core.exe')
 
 FEEDBACK_SOURCE_ROOT = os.path.join('.', 'FeedbackSite')
 FEEDBACK_HTML_SOURCE_PATH = os.path.join(FEEDBACK_SOURCE_ROOT, 'feedback.html')
@@ -75,6 +76,8 @@ def build_client_executable():
     commands = [
         '"%s" sign /t http://timestamp.digicert.com /f "%s" "%s"\n' % (
           signtool_filename, CODE_SIGNING_PFX_FILENAME, POLIPO_EXECUTABLE_FILENAME),
+        '"%s" sign /t http://timestamp.digicert.com /f "%s" "%s"\n' % (
+          signtool_filename, CODE_SIGNING_PFX_FILENAME, CORE_EXECUTABLE_FILENAME),
         'msbuild "%s" /v:quiet /t:Rebuild /p:Configuration=Release\n' % (
           CLIENT_SOLUTION_FILENAME,),
         '"%s" -qq "%s"\n' % (UPX_FILENAME, EXECUTABLE_FILENAME),
@@ -222,7 +225,8 @@ def build_client(
             [BANNER_FILENAME,
              EMAIL_BANNER_FILENAME,
              FEEDBACK_HTML_PATH,
-             POLIPO_EXECUTABLE_FILENAME])
+             POLIPO_EXECUTABLE_FILENAME,
+             CORE_EXECUTABLE_FILENAME])
 
         # Copy custom email banner from Data to source tree
         # (there's only one custom email banner for all sponsors)
