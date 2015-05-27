5f205f24c0a83442a16afc88fac6310286f8006e
Kruger <none@none>
2015-05-27 14:08:40 -0400

    Increment class_version.
    --HG--
    branch : campaign-custom-platform-sets

--- Automation/psi_ops.py
+++ Automation/psi_ops.py
@@ -350,7 +350,7 @@ class PsiphonNetwork(psi_ops_cms.PersistentObject):
         if initialize_plugins:
             self.initialize_plugins()
 
-    class_version = '0.30'
+    class_version = '0.31'
 
     def upgrade(self):
         if cmp(parse_version(self.version), parse_version('0.1')) < 0:
