dc666db0a6c63658dce851dbbbee70fcc22feb0d
Draven J. <D.Johnson@Psiphon.ca>
2016-06-08 13:09:43 -0400

    Change/Delete some testing stuff. New method tested
    --HG--
    branch : imageid-test

--- Automation/psi_digitalocean_apiv2.py
+++ Automation/psi_digitalocean_apiv2.py
@@ -491,7 +491,6 @@ def launch_new_server(digitalocean_account, _):
             instance of a psinet server
     """
     digitalocean_account.base_id = '17784624'
-    print(digitalocean_account)
     try:
         Droplet = collections.namedtuple('Droplet', ['name', 'region', 'image',
                                                      'size', 'backups'])
