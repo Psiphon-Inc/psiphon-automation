3467c7b6c6682f7405ed277988d24d39ddfb223a
rhynes <none@none>
2016-10-20 12:05:55 -0400

    Restore psi_limit_load CPU trigger for TCS (with a new threshold)
    --HG--
    branch : TCS-automation

--- Automation/psi_ops_install.py
+++ Automation/psi_ops_install.py
@@ -1262,10 +1262,21 @@ def install_TCS_psi_limit_load(host):
     script = '''
 #!/bin/bash
 
+threshold_load_per_cpu=1
 threshold_mem=10
 threshold_swap=20
 
 while true; do
+
+    loaded_cpu=0
+    num_cpu=`grep 'model name' /proc/cpuinfo | wc -l`
+    threshold_cpu=$(($threshold_load_per_cpu * $num_cpu))
+    load_cpu=`uptime | cut -d , -f 4 | cut -d : -f 2 | awk -F \. '{print $1}'`
+    if [ "$load_cpu" -ge "$threshold_cpu" ]; then
+        loaded_cpu=1
+        logger psi_limit_load: CPU load threshold reached.
+        break
+    fi
 
     free=$(free | grep "buffers/cache" | awk '{print $4/($3+$4) * 100.0}')
     loaded_mem=$(echo "$free<$threshold_mem" | bc)
@@ -1286,7 +1297,7 @@ while true; do
     break
 done
 
-if [ $loaded_mem -eq 1 ] || [ $loaded_swap -eq 1 ]; then
+if [ $loaded_cpu -eq 1 ] || [ $loaded_mem -eq 1 ] || [ $loaded_swap -eq 1 ]; then
     iptables -D FORWARD -o docker0 -j PSI_LIMIT_LOAD
     iptables -I FORWARD -o docker0 -j PSI_LIMIT_LOAD
     %s
