370fdb93e06e0907a2dc97bc3aa7173b36d328f9
Draven Johnson <d.johnson@psiphon.ca>
2016-10-24 16:22:03 -0400

    Change to new digitalocean image id
    --HG--
    branch : TCS-automation

--- Automation/psi_digitalocean_apiv2.py
+++ Automation/psi_digitalocean_apiv2.py
@@ -511,7 +511,7 @@ def launch_new_server(digitalocean_account, is_TCS, _):
             instance of a psinet server
     """
 
-    digitalocean_account.base_id = '17784624' if not is_TCS else '20432611'
+    digitalocean_account.base_id = '17784624' if not is_TCS else '20484114'
     try:
         Droplet = collections.namedtuple('Droplet', ['name', 'region', 'image',
                                                      'size', 'backups'])
