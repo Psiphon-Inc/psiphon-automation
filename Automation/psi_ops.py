70f03b7b639439747c0ff08fde6a7349c4ffd9e7
rhynes <none@none>
2017-01-11 12:29:15 -0500

    Add automation support for deploying session ticket protocol servers
    --HG--
    branch : session-ticket-protocol

--- Automation/psi_ops.py
+++ Automation/psi_ops.py
@@ -221,14 +221,14 @@ def ServerCapabilities():
     for capability in ('handshake', 'VPN', 'SSH', 'OSSH'):
         capabilities[capability] = True
     # These are disabled by default
-    for capability in ('ssh-api-requests', 'FRONTED-MEEK', 'UNFRONTED-MEEK'):
+    for capability in ('ssh-api-requests', 'FRONTED-MEEK', 'UNFRONTED-MEEK', 'UNFRONTED-MEEK-SESSION-TICKET'):
         capabilities[capability] = False
     return capabilities
 
 
 def copy_server_capabilities(caps):
     capabilities = {}
-    for capability in ('handshake', 'ssh-api-requests', 'VPN', 'SSH', 'OSSH', 'FRONTED-MEEK', 'UNFRONTED-MEEK'):
+    for capability in ('handshake', 'ssh-api-requests', 'VPN', 'SSH', 'OSSH', 'FRONTED-MEEK', 'UNFRONTED-MEEK', 'UNFRONTED-MEEK-SESSION-TICKET'):
         capabilities[capability] = caps[capability]
     return capabilities
 
@@ -626,6 +626,14 @@ class PsiphonNetwork(psi_ops_cms.PersistentObject):
         if cmp(parse_version(self.version), parse_version('0.39')) < 0:
             self.__default_sponsor_id = None
             self.version = '0.39'
+        if cmp(parse_version(self.version), parse_version('0.40')) < 0:
+            self.version = '0.40'
+            for server in self.__servers.itervalues():
+                if server.capabilities:
+                    server.capabilities['UNFRONTED-MEEK-SESSION-TICKET'] = False
+            for server in self.__deleted_servers.itervalues():
+                if server.capabilities:
+                    server.capabilities['UNFRONTED-MEEK-SESSION-TICKET'] = False
 
     def initialize_plugins(self):
         for plugin in plugins:
@@ -1448,6 +1456,7 @@ class PsiphonNetwork(psi_ops_cms.PersistentObject):
         server.capabilities['OSSH'] = False
         server.capabilities['FRONTED-MEEK'] = False
         server.capabilities['UNFRONTED-MEEK'] = False
+        server.capabilities['UNFRONTED-MEEK-SESSION-TICKET'] = False
         host = self.__hosts[server.host_id]
         servers = [s for s in self.__servers.itervalues() if s.host_id == server.host_id]
         if host.is_TCS:
@@ -1635,6 +1644,7 @@ class PsiphonNetwork(psi_ops_cms.PersistentObject):
         server = self.__servers[server_id]
         host = self.__hosts[server.host_id]
         assert(host.meek_server_port == None)
+        assert(host.is_TCS)
 
         server.capabilities['handshake'] = False
         server.capabilities['VPN'] = False
@@ -1643,6 +1653,21 @@ class PsiphonNetwork(psi_ops_cms.PersistentObject):
         server.capabilities['FRONTED-MEEK'] = False
         server.capabilities['UNFRONTED-MEEK'] = True
         self.setup_meek_parameters_for_host(host, 80)
+        self.install_meek_for_host(host)
+
+    def setup_unfronted_meek_session_ticket_for_server(self, server_id):
+        server = self.__servers[server_id]
+        host = self.__hosts[server.host_id]
+        assert(host.meek_server_port == None)
+
+        server.capabilities['handshake'] = False
+        server.capabilities['VPN'] = False
+        server.capabilities['SSH'] = False
+        server.capabilities['OSSH'] = False
+        server.capabilities['FRONTED-MEEK'] = False
+        server.capabilities['UNFRONTED-MEEK'] = False
+        server.capabilities['UNFRONTED-MEEK-SESSION-TICKET'] = True
+        self.setup_meek_parameters_for_host(host, 443)
         self.install_meek_for_host(host)
 
     def setup_meek_parameters_for_host(self, host, meek_server_port):
@@ -3023,7 +3048,7 @@ class PsiphonNetwork(psi_ops_cms.PersistentObject):
 
         extended_config['sshObfuscatedPort'] = int(server.ssh_obfuscated_port) if server.ssh_obfuscated_port else 0
         # Use the latest alternate port unless tunneling through meek
-        if server.alternate_ssh_obfuscated_ports and not (server.capabilities['FRONTED-MEEK'] or server.capabilities['UNFRONTED-MEEK']):
+        if server.alternate_ssh_obfuscated_ports and not (server.capabilities['FRONTED-MEEK'] or server.capabilities['UNFRONTED-MEEK'] or server.capabilities['UNFRONTED-MEEK-SESSION-TICKET']):
             extended_config['sshObfuscatedPort'] = int(server.alternate_ssh_obfuscated_ports[-1])
         extended_config['sshObfuscatedKey'] = server.ssh_obfuscated_key if server.ssh_obfuscated_key else ''
 
@@ -3899,8 +3924,8 @@ if __name__ == "__main__":
     parser.add_option("-r", "--read-only", dest="readonly", action="store_true",
                       help="don't lock the network object")
     parser.add_option("-t", "--test", dest="test", action="append",
-                      choices=('handshake', 'VPN', 'OSSH', 'SSH', 'FRONTED-MEEK-OSSH', 'FRONTED-MEEK-HTTP-OSSH', 'UNFRONTED-MEEK-OSSH', 'UNFRONTED-MEEK-HTTPS-OSSH'),
-                      help="specify once for each of: handshake, VPN, OSSH, SSH, FRONTED-MEEK-OSSH, FRONTED-MEEK-HTTP-OSSH, UNFRONTED-MEEK-OSSH, UNFRONTED-MEEK-HTTPS-OSSH")
+                      choices=('handshake', 'VPN', 'OSSH', 'SSH', 'FRONTED-MEEK-OSSH', 'FRONTED-MEEK-HTTP-OSSH', 'UNFRONTED-MEEK-OSSH', 'UNFRONTED-MEEK-HTTPS-OSSH', 'UNFRONTED-MEEK-SESSION-TICKET-OSSH'),
+                      help="specify once for each of: handshake, VPN, OSSH, SSH, FRONTED-MEEK-OSSH, FRONTED-MEEK-HTTP-OSSH, UNFRONTED-MEEK-OSSH, UNFRONTED-MEEK-HTTPS-OSSH, UNFRONTED-MEEK-SESSION-TICKET-OSSH")
     parser.add_option("-u", "--update-routes", dest="updateroutes", action="store_true",
                       help="update external signed routes files")
     parser.add_option("-p", "--prune", dest="prune", action="store_true",
