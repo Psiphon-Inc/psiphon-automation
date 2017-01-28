ac902a09b31af8d1bd214af0ff231df311f2e4fc
Draven J. <d.johnson@psiphon.ca>
2017-01-28 01:12:21 -0800

    Revert baseimage back to old working one.

--- Automation/psi_digitalocean_apiv2.py
+++ Automation/psi_digitalocean_apiv2.py
@@ -511,7 +511,7 @@ def launch_new_server(digitalocean_account, is_TCS, _):
             instance of a psinet server
     """
 
-    base_id = '17784624' if not is_TCS else '22464614'
+    base_id = '17784624' if not is_TCS else '20668187'
     try:
         Droplet = collections.namedtuple('Droplet', ['name', 'region', 'image',
                                                      'size', 'backups'])
