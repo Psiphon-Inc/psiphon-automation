528beb4004662b43563f9d7659ee9cee9b1925b3
Draven Johnson <d.johnson@psiphon.ca>
2017-02-23 14:18:17 -0500

    Start SSH service after restore the sshd_config

--- Automation/psi_ops.py
+++ Automation/psi_ops.py
@@ -2083,6 +2083,7 @@ class PsiphonNetwork(psi_ops_cms.PersistentObject):
                     remote_fliepath = os.path.join(remote_path, filename)
                     # put file
                     ssh.put_file(local_path, remote_fliepath)
+            ssh.exec_command('service ssh restart')
         else:
             print('Action is not supported, please use "backup" or "restore"')
             return
