eb47affe5be3ffec08693e39389f06ddd2fd2cc9
Adam Kruger <a.kruger@psiphon.ca>
2017-07-24 10:30:09 -0400

    Increase prune threshold.

--- Automation/psi_ops.py
+++ Automation/psi_ops.py
@@ -1544,7 +1544,7 @@ class PsiphonNetwork(psi_ops_cms.PersistentObject):
         number_disabled = 0
         for server in servers:
             users_on_host = self.__count_users_on_host(server.host_id)
-            if users_on_host <= 10:
+            if users_on_host <= 15:
                 self.remove_host(server.host_id)
                 number_removed += 1
             elif users_on_host < 50:
