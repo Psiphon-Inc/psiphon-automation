7bbe678ae7bb2f74edc061e8ac5e0233309b97bb
Adam Kruger <a.kruger@psiphon.ca>
2018-01-16 21:33:07 -0500

    Update to new droplet plan.

--- Automation/psi_digitalocean_apiv2.py
+++ Automation/psi_digitalocean_apiv2.py
@@ -547,7 +547,7 @@ def launch_new_server(digitalocean_account, is_TCS, _):
         if not unicode(digitalocean_account.base_size_slug) in [unicode(s.slug) for s in droplet_sizes]:
             raise 'Size slug not found'
 
-        Droplet.size = '2gb'
+        Droplet.size = 's-2vcpu-4gb'
 
         droplet_regions = do_mgr.get_all_regions()
         common_regions = list(set([r.slug for r in droplet_regions if r.available])
