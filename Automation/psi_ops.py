1acf45be10a3ae6cb2a9d3d44f5f75a0831e66ea
Draven Johnson <d.johnson@psiphon.ca>
2019-07-30 14:08:32 -0400

    Fix the new devops DB name issue
    --HG--
    branch : devops_compartmental

--- Automation/psi_ops.py
+++ Automation/psi_ops.py
@@ -3192,7 +3192,7 @@ class PsiphonNetwork(psi_ops_cms.PersistentObject):
         try:
             temp_file.write(self.__compartmentalize_data_for_devops_server())
             temp_file.close()
-            psi_ops_cms.import_document(temp_file.name, True)
+            psi_ops_cms.import_document(temp_file.name, False, True)
             self.__stats_server_account.log('pushed')
         finally:
             try:
@@ -3208,7 +3208,7 @@ class PsiphonNetwork(psi_ops_cms.PersistentObject):
         try:
             temp_file.write(self.__compartmentalize_data_for_stats_server())
             temp_file.close()
-            psi_ops_cms.import_document(temp_file.name, True)
+            psi_ops_cms.import_document(temp_file.name, True, False)
             self.__stats_server_account.log('pushed')
         finally:
             try:
