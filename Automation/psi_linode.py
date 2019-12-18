08caeffc4fea272854e96a9ea87f9de604f5d85c
Kruger <none@none>
2019-12-18 16:22:50 -0500

    Add AU and IN linode regions.

--- Automation/psi_linode.py
+++ Automation/psi_linode.py
@@ -42,16 +42,18 @@ def wait_while_condition(condition, max_wait_seconds, description):
 
 def get_region(datacenter_id):
     # from linode_api.avail_datacenters():
-    # [{u'DATACENTERID': 2, u'LOCATION': u'Dallas, TX, USA'},
-    #  {u'DATACENTERID': 3, u'LOCATION': u'Fremont, CA, USA'},
-    #  {u'DATACENTERID': 4, u'LOCATION': u'Atlanta, GA, USA'},
-    #  {u'DATACENTERID': 6, u'LOCATION': u'Newark, NJ, USA'},
-    #  {u'DATACENTERID': 7, u'LOCATION': u'London, England, UK'},
+    # [{u'ABBR': u'dallas', u'DATACENTERID': 2, u'LOCATION': u'Dallas, TX, USA'},
+    #  {u'ABBR': u'fremont', u'DATACENTERID': 3, u'LOCATION': u'Fremont, CA, USA'},
+    #  {u'ABBR': u'atlanta', u'DATACENTERID': 4, u'LOCATION': u'Atlanta, GA, USA'},
+    #  {u'ABBR': u'newark', u'DATACENTERID': 6, u'LOCATION': u'Newark, NJ, USA'},
+    #  {u'ABBR': u'london', u'DATACENTERID': 7, u'LOCATION': u'London, England, UK'},
     #  {u'DATACENTERID': 8, u'LOCATION': u'Tokyo, JP'},
-    #  {u'DATACENTERID': 9, u'LOCATION': u'Singapore, SG', u'ABBR': u'singapore'},
-    #  {u'DATACENTERID': 10, u'LOCATION': u'Frankfurt, DE', u'ABBR': u'frankfurt'},
-    #  {u'DATACENTERID': 11, u'LOCATION': u'Tokyo 2, JP', u'ABBR': u'shinagawa1'},
-    #  {u'DATACENTERID': 15, u'LOCATION': u'Toronto, Ontario, CAN', u'ABBR': u'tor1'}]
+    #  {u'ABBR': u'singapore', u'DATACENTERID': 9, u'LOCATION': u'Singapore, SG'},
+    #  {u'ABBR': u'frankfurt', u'DATACENTERID': 10, u'LOCATION': u'Frankfurt, DE'},
+    #  {u'ABBR': u'shinagawa1', u'DATACENTERID': 11, u'LOCATION': u'Tokyo 2, JP'},
+    #  {u'ABBR': u'mum1', u'DATACENTERID': 14, u'LOCATION': u'Mumbai, India'},
+    #  {u'ABBR': u'tor1', u'DATACENTERID': 15, u'LOCATION': u'Toronto, Ontario, CAN'},
+    #  {u'ABBR': u'syd1', u'DATACENTERID': 16, u'LOCATION': u'Sydney, NSW, Australia'}]
     if datacenter_id in [2, 3, 4, 6]:
         return 'US'
     if datacenter_id in [7]:
@@ -62,8 +64,12 @@ def get_region(datacenter_id):
         return 'SG'
     if datacenter_id in [10]:
         return 'DE'
+    if datacenter_id in [14]:
+        return 'IN'
     if datacenter_id in [15]:
         return 'CA'
+    if datacenter_id in [16]:
+        return 'AU'
     return ''
 
 def create_linode(linode_api):
