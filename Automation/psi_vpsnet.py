8a021bb74347b5f08ffdb5c361d4d27ff28756ee
Draven Johnson <d.johnson@psiphon.ca>
2020-01-18 13:47:57 -0500

    Base image update to upgrade filebeat

--- Automation/psi_vpsnet.py
+++ Automation/psi_vpsnet.py
@@ -182,7 +182,7 @@ def launch_new_server(vpsnet_account, is_TCS, _, multi_ip=False, datacenter_city
     """
 
     # TODO-TCS: select base image based on is_TCS flag
-    base_image_id = '9258' # For VPS
+    base_image_id = '9388' # For VPS
     # base_image_id = '8850' # For Cloud Server
 
     try:
