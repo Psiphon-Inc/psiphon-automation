7d88653464908a5579f40d1d06b81086613ae457
Adam Kruger <a.kruger@psiphon.ca>
2022-05-19 10:42:59 -0400

    Include more permanent servers in server lists

--- Automation/psi_ops.py
+++ Automation/psi_ops.py
@@ -4054,7 +4054,7 @@ class PsiphonNetwork(psi_ops_cms.PersistentObject):
             servers = [server for server in self.__servers.itervalues()
                        if (server.propagation_channel_id == propagation_channel_id and
                            (server.is_permanent or (server.is_embedded and include_propagation_servers)))
-                       or (not test and (server.id in permanent_server_ids[0:200]))]
+                       or (not test and (server.id in permanent_server_ids[0:400]))]
         else:
             # discovery case
             if not discovery_date:
