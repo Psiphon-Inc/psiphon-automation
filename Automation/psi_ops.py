6f0eed680a0f1188d5db8b780cbe44d186ad2f0f
Draven Johnson <d.johnson@psiphon.ca>
2023-03-09 18:08:36 -0500

    Update psi_ops.py
    Update Class version

--- Automation/psi_ops.py
+++ Automation/psi_ops.py
@@ -467,7 +467,7 @@ class PsiphonNetwork(psi_ops_cms.PersistentObject):
         if initialize_plugins:
             self.initialize_plugins()
 
-    class_version = '0.68'
+    class_version = '0.69'
 
     def upgrade(self):
         if cmp(parse_version(self.version), parse_version('0.1')) < 0:
