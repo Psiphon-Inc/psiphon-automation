0c9559b07ec1d0a581b5f699361c6653f60b4cdb
adamkruger <a.kruger@psiphon.ca>
2023-04-10 21:14:30 -0400

    Increase discovery window

--- Automation/psi_ops.py
+++ Automation/psi_ops.py
@@ -1951,8 +1951,8 @@ class PsiphonNetwork(psi_ops_cms.PersistentObject):
         today = datetime.datetime(now.year, now.month, now.day)
         tomorrow = today + datetime.timedelta(days=1)
 
-        # Use a default 2 day discovery date range.
-        new_discovery_date_range = (tomorrow, tomorrow + datetime.timedelta(days=2))
+        # Use a default 5 day discovery date range.
+        new_discovery_date_range = (tomorrow, tomorrow + datetime.timedelta(days=5))
         # Use a default 15 day osl discovery date range.
         new_osl_discovery_date_range = (today, today + datetime.timedelta(days=15))
 
