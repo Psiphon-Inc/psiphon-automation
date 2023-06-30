2d3b1f6aa03eaf8988475aa2211abee546682270
Adam Kruger <a.kruger@psiphon.ca>
2023-06-30 09:03:37 -0400

    Pass byte objects into zlib.compress

--- Automation/psi_ops.py
+++ Automation/psi_ops.py
@@ -3248,7 +3248,7 @@ class PsiphonNetwork(psi_ops_cms.PersistentObject):
                     # the entire file is compressed instead of just the payload
                     # because the compressed payload would need to be base64 encoded
                     # in the json contents of the file, losing compression
-                    remote_server_list_compressed = zlib.compress(remote_server_list)
+                    remote_server_list_compressed = zlib.compress(remote_server_list.encode())
 
                     # Build for each client platform
 
@@ -3488,7 +3488,7 @@ class PsiphonNetwork(psi_ops_cms.PersistentObject):
             empty_osl_registry = zlib.compress(psi_ops_crypto_tools.make_signed_data(
                     self.__get_remote_server_list_signing_key_pair().pem_key_pair,
                     REMOTE_SERVER_SIGNING_KEY_PAIR_PASSWORD,
-                    base64.b64encode('{"FileSpecs" : []}'.encode())))
+                    base64.b64encode('{"FileSpecs" : []}'.encode())).encode())
 
             for sponsor in self.__sponsors.values():
                 for campaign in sponsor.campaigns:
