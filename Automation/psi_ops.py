00e2c9f4798a74ebad9a089721d1a00a84a6468f
Adam Kruger <a.kruger@psiphon.ca>
2023-08-02 00:15:54 -0400

    Syntax fix for python 3

--- Automation/psi_ops.py
+++ Automation/psi_ops.py
@@ -912,7 +912,7 @@ class PsiphonNetwork(psi_ops_cms.PersistentObject):
             self.__oci_account = OracleAccount()
             self.version = '0.70'
         if cmp(parse_version(self.version), parse_version('0.71')) < 0:
-            for host in self.__hosts.values() + list(self.__deleted_hosts) + list(self.__hosts_to_remove_from_providers):
+            for host in list(self.__hosts.values()) + list(self.__deleted_hosts) + list(self.__hosts_to_remove_from_providers):
                 host.ipmi_ip_address = ""
                 host.ipmi_username = ""
                 host.ipmi_password = ""
