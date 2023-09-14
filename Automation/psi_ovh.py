1c41f5479b5edb4da4d8746f3ed3beef82846bfb
Draven Johnson <d.johnson@psiphon.ca>
2023-09-13 21:59:11 -0400

    Order Ips take a list of servers now

--- Automation/psi_ovh.py
+++ Automation/psi_ovh.py
@@ -56,51 +56,56 @@ class PsiOVH:
 
     return checkout_res
 
-  def order_new_ip_addresses(self, destination_server_id=None, quantity=5):
+  def order_new_ip_addresses(self, destination_server_ids=None, quantity=5):
     cart_id = self.create_new_order_cart()
 
-    server = self.client.get('/dedicated/server/{serverId}'.format(serverId=destination_server_id))
+    for destination_server_id in destination_server_ids:
+      server = self.client.get('/dedicated/server/{serverId}'.format(serverId=destination_server_id))
 
-    country = self.get_region_by_datacenter(server['datacenter'])
-    print("Using IP Country: {}".format(country))
+      country = self.get_region_by_datacenter(server['datacenter'])
+      print("Using IP Country: {}".format(country))
 
-    if country in ['AU', 'CA', 'IN', 'SG']:
-      plan_code = 'ip-failover-arin'
-    elif country in ['BE', 'CH', 'CZ', 'DE', 'ES', 'FI', 'FR', 'IE', 'IT', 'LT', 'NL', 'PL', 'PT', 'UK']:
-      plan_code = 'ip-failover-ripe'
-    else:
-      print("IP Country not available to match plan_code...")
+      if country in ['AU', 'CA', 'IN', 'SG']:
+        plan_code = 'ip-failover-arin'
+      elif country in ['BE', 'CH', 'CZ', 'DE', 'ES', 'FI', 'FR', 'IE', 'IT', 'LT', 'NL', 'PL', 'PT', 'UK']:
+        plan_code = 'ip-failover-ripe'
+      elif country in ['GB']:
+        plan_code = 'ip-failover-ripe'
+      else:
+        print("IP Country not available to match plan_code...")
 
-    print("Using PlanCode: {}".format(plan_code))
-    ip_order = self.client.post('/order/cart/{cartId}/ip'.format(cartId=cart_id),
-                           duration='P1M',
-                           planCode=plan_code,
-                           pricingMode='default',
-                           quantity=quantity
-                           )
+      print("Using PlanCode: {}".format(plan_code))
+      ip_order = self.client.post('/order/cart/{cartId}/ip'.format(cartId=cart_id),
+                             duration='P1M',
+                             planCode=plan_code,
+                             pricingMode='default',
+                             quantity=quantity
+                             )
 
-    item_id = ip_order['itemId']
+      item_id = ip_order['itemId']
 
-    ip_available_country = self.client.get('/order/cart/{cartId}/item/{itemId}/requiredConfiguration'.format(cartId=cart_id, itemId=ip_order['itemId']))[0]['allowedValues']
-    print(ip_available_country)
-    if country in ip_available_country:
-      ip_country = country
-    else:
-      print("IP Country incorrect..")
+      ip_available_country = self.client.get('/order/cart/{cartId}/item/{itemId}/requiredConfiguration'.format(cartId=cart_id, itemId=ip_order['itemId']))[0]['allowedValues']
 
-    ip_config_country = self.client.post('/order/cart/{cartId}/item/{itemId}/configuration'.format(cartId=cart_id, itemId=item_id),
-                                 label='country',
-                                 value=ip_country
-                                 )
+      if country in ip_available_country:
+        ip_country = country
+      elif country == 'GB':
+        ip_country = 'UK'
+      else:
+        print("IP Country incorrect..")
 
-    if plan_code == 'ip-failover-ripe':
-      print("RIPE IP require destination servers, region: {}, destination: {}".format(ip_country, destination_server_id))
-      ip_config_destination = self.client.post('/order/cart/{cartId}/item/{itemId}/configuration'.format(cartId=cart_id, itemId=item_id),
-                                 label='destination',
-                                 value=destination_server_id
-                                 )
+      ip_config_country = self.client.post('/order/cart/{cartId}/item/{itemId}/configuration'.format(cartId=cart_id, itemId=item_id),
+                                   label='country',
+                                   value=ip_country
+                                   )
 
-    print("Success setup cart order, CartID:{} and ItemID:{}".format(cart_id, item_id))
+      if plan_code == 'ip-failover-ripe':
+        print("RIPE IP require destination servers, region: {}, destination: {}".format(ip_country, destination_server_id))
+        ip_config_destination = self.client.post('/order/cart/{cartId}/item/{itemId}/configuration'.format(cartId=cart_id, itemId=item_id),
+                                   label='destination',
+                                   value=destination_server_id
+                                   )
+
+      print("Success setup cart order, CartID:{} and ItemID:{}".format(cart_id, item_id))
 
     checkout_order = self.checkout_order_cart(cart_id)
 
