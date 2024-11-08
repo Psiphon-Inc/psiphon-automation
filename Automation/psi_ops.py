a4ab706c7ef98d808a29b9d86a62cbaf634732c3
Adam Kruger <a.kruger@psiphon.ca>
2024-11-08 10:32:10 -0500

    All TLS servers have passthrough v2

--- Automation/psi_ops.py
+++ Automation/psi_ops.py
@@ -2657,9 +2657,8 @@ class PsiphonNetwork(psi_ops_cms.PersistentObject):
             supports_passthrough = psi_ops_deploy.server_supports_passthrough(server, host)
             if supports_passthrough and len(self.__passthrough_addresses) > 0:
                 host.passthrough_address = random.choice(self.__passthrough_addresses) + ':' + (str(tls_port) if capabilities['TLS'] else str(host.meek_server_port))
-                if ((capabilities['TLS'] and int(tls_port) == 443) or
-                    (capabilities['UNFRONTED-MEEK'] and int(host.meek_server_port) == 443) or
-                    (capabilities['UNFRONTED-MEEK-SESSION-TICKET'] and int(host.meek_server_port) == 443)) and not host.passthrough_version and random.random() > 0.25:
+                if (capabilities['TLS'] or
+                    (int(host.meek_server_port) == 443 and not host.passthrough_version and random.random() > 0.25):
                     host.passthrough_version = 2
 
             self.setup_server(host, [server])
