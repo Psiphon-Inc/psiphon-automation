93a6430ee20b4d61e98e0e57c0ae25ba6a34672e
adamkruger <a.kruger@psiphon.ca>
2025-11-12 20:35:24 -0500

    Support new datacenter

--- Automation/psi_digitalocean_apiv2.py
+++ Automation/psi_digitalocean_apiv2.py
@@ -195,23 +195,27 @@ def get_datacenter_region(region):
             2-digit country code
     """
     '''
-        nyc1 New York 1
-        ams1 Amsterdam 1
-        sfo1 San Francisco
-        nyc2 New York 2
         ams2 Amsterdam 2
-        sgp1 Singapore 1
-        lon1 London 1
-        nyc3 New York 3
         ams3 Amsterdam 3
+        atl1 Atlanta 1
+        blr1 Bangalore 1
         fra1 Frankfurt 1
-        tor1 Toronto 1
-        blr1 Banglore 1
+        lon1 London 1
+        nyc1 New York 1
+        nyc2 New York 2
+        nyc3 New York 3
+        sfo1 San Francisco 1
+        sfo2 San Francisco 2
+        sfo3 San Francisco 3
+        sgp1 Singapore 1
         syd1 Sydney 1
+        tor1 Toronto 1
     '''
     if 'nyc' in region:
         return 'US'
     if 'sfo' in region:
+        return 'US'
+    if 'atl' in region:
         return 'US'
     if 'ams' in region:
         return 'NL'
