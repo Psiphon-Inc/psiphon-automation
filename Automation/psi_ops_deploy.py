483095c4846de41d70705dc24ff178e42e52768d
adamkruger <a.kruger@psiphon.ca>
2026-08-06 21:55:00 -0400

    Remove unused parameters

--- Automation/psi_ops_deploy.py
+++ Automation/psi_ops_deploy.py
@@ -713,17 +713,6 @@ def deploy_TCS_data(ssh, host, host_data, TCS_traffic_rules_set, TCS_OSL_config,
 
     put_file_with_content(ssh, TCS_OSL_config, TCS_OSL_CONFIG_FILE_NAME)
 
-    tactics_request_public_key = ''
-    tactics_request_private_key = ''
-    tactics_request_obfuscated_key = ''
-
-    if host.tactics_request_public_key != None:
-        tactics_request_public_key = host.tactics_request_public_key
-    if host.tactics_request_private_key != None:
-        tactics_request_private_key = host.tactics_request_private_key
-    if host.tactics_request_obfuscated_key != None:
-        tactics_request_obfuscated_key = host.tactics_request_obfuscated_key
-
     put_file_with_content(ssh, TCS_tactics_config_template, TCS_TACTICS_CONFIG_FILE_NAME)
 
     put_file_with_content(ssh, TCS_blocklist_csv, TCS_BLOCKLIST_CSV_FILE_NAME)
